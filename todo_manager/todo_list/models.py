0fb4951755e7369a9edfa5315fe8c4261ba38cd3
Aleksandr <gggols32@gmail.com>
2024-09-29 22:50:06 +0300

    Defined __str__ method for To do item model

--- todo_manager/todo_list/models.py
+++ todo_manager/todo_list/models.py
@@ -5,5 +5,8 @@ class ToDoItem(models.Model):
   title = models.CharField(max_length=250)
   is_done = models.BooleanField(default=False)
 
+  def __str__(self):
+    return self.title
+
   class Meta:
     verbose_name = "To do item"
