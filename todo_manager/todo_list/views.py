965cef6eaf425922bf86a9f0840011cdcc056b23
Aleksandr <gggols32@gmail.com>
2024-09-30 16:00:54 +0300

    Upgrage To do List View: Getting items from DB

--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .models import ToDoItem
 
 # Create your views here.
 def index_view(request: HttpRequest) -> HttpResponse:
-  return render(request, "todo_list/index.html")
+  todo_items = ToDoItem.objects.order_by("-id").all()
+  return render(
+    request, 
+    "todo_list/index.html",
+    context={"todo_items": todo_items}
+  )
