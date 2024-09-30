937571a93baf6e7478e756cd091c26ec5581b6fb
Aleksandr <gggols32@gmail.com>
2024-09-30 15:40:50 +0300

    Created To do List Func Based View

--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from . import views
+
 app_name = "todo_list"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="todo_list/index.html"), name="index"),
+    path("", views.index_view, name="index"),
 ]
